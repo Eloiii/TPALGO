6ecce743d78e0dc26cb12cd8f8b8a84f7f9d21e9
Eloi Charra <eloi.charra@etu.univ-grenoble-alpes.fr>
2022-04-05 14:39:24 +0200

    doing clemax

--- TP2/a234.c
+++ TP2/a234.c
@@ -4,102 +4,92 @@
 
 #include "a234.h"
 
-#define max(a,b) ((a)>(b)?(a):(b))
+#define max(a, b) ((a)>(b)?(a):(b))
 
 
-int hauteur (Arbre234 a)
-{
-  int h0, h1, h2, h3 ;
-  
-  if (a == NULL)
-    return 0 ;
+int hauteur(Arbre234 a) {
+    int h0, h1, h2, h3;
 
-  if (a->t == 0)
-    return 0 ;
+    if (a == NULL)
+        return 0;
 
-  h0 = hauteur (a->fils [0]) ;
-  h1 = hauteur (a->fils [1]) ;
-  h2 = hauteur (a->fils [2]) ;
-  h3 = hauteur (a->fils [3]) ;
+    if (a->t == 0)
+        return 0;
 
-  return 1 + max (max (h0,h1),max (h2,h3)) ;
-} 
+    h0 = hauteur(a->fils[0]);
+    h1 = hauteur(a->fils[1]);
+    h2 = hauteur(a->fils[2]);
+    h3 = hauteur(a->fils[3]);
 
-int NombreCles (Arbre234 a)
-{
-  /*
-     Retourne le nombre de cles presentes dans l'arbre a
-  */
-  
-  return 0 ;
+    return 1 + max (max(h0, h1), max(h2, h3));
 }
 
-int CleMax (Arbre234 a)
-{
-  /*
-     plus grande cle de l'arbre a
-  */
-  
-  return 0 ;
+int NombreCles(Arbre234 a) {
+    /*
+       Retourne le nombre de cles presentes dans l'arbre a
+    */
+
+    return 0;
 }
 
-int CleMin (Arbre234 a)
-{
-  /*
-     Retourne plus petite cle de l'arbre a
-  */
-
-  return 0 ;
+int CleMax(Arbre234 a) {
+    if (a == NULL) {
+        return NULL;
+    } else if (a->t < 2) {
+        return a->cles[1];
+    } else {
+        if(a->t == 2)
+            return CleMax(a->fils[2]);
+        else
+            return CleMax(a->fils[3]);
+    }
 }
 
-Arbre234 RechercherCle (Arbre234 a, int cle)
-{
-  /*
-     rechercher si la cle a est presente dans
-     l'arbre a. Si oui, retourne le noeud ou se trouve la cle.
-  */
+int CleMin(Arbre234 a) {
+    /*
+       Retourne plus petite cle de l'arbre a
+    */
 
-  return NULL ;
+    return 0;
 }
 
-void AnalyseStructureArbre (Arbre234 a, int *feuilles, int *noeud2, int *noeud3, int *noeud4)
-{
-  /* 
-     calculer le nombre de feuilles, de 2-noeuds, 3-noeuds,et 4-noeuds
-  */
-}
-
-Arbre234 noeud_max (Arbre234 a)
-{
-  /*
-    Retourne le noeud avec la somme maximale des cles internes
-  */
-  
-  return NULL ;
+Arbre234 RechercherCle(Arbre234 a, int cle) {
 }
 
 
-void Afficher_Cles_Largeur (Arbre234 a)
-{
-  /*
-    Afficher le cles de l'arbre a avec
-    un parcours en largeur
-  */
-
-  return ;
+void AnalyseStructureArbre(Arbre234 a, int *feuilles, int *noeud2, int *noeud3, int *noeud4) {
+    /*
+       calculer le nombre de feuilles, de 2-noeuds, 3-noeuds,et 4-noeuds
+    */
 }
 
-void Affichage_Cles_Triees_Recursive (Arbre234 a)
-{
-  /* 
-     Afficher les cles en ordre croissant
-     Cette fonction sera recursive
-  */
-     
+Arbre234 noeud_max(Arbre234 a) {
+    /*
+      Retourne le noeud avec la somme maximale des cles internes
+    */
+
+    return NULL;
 }
 
-void Affichage_Cles_Triees_NonRecursive (Arbre234 a)
-{
+
+void Afficher_Cles_Largeur(Arbre234 a) {
+    /*
+      Afficher le cles de l'arbre a avec
+      un parcours en largeur
+    */
+
+    return;
+}
+
+void Affichage_Cles_Triees_Recursive(Arbre234 a) {
+    /*
+       Afficher les cles en ordre croissant
+       Cette fonction sera recursive
+    */
+
+}
+
+void Affichage_Cles_Triees_NonRecursive(Arbre234 a) {
     /* 
      Afficher les cles en ordre croissant
      Cette fonction ne sera pas recursive
@@ -109,32 +99,29 @@ void Affichage_Cles_Triees_NonRecursive (Arbre234 a)
 }
 
 
-void Detruire_Cle (Arbre234 *a, int cle)
-{
-  /*
-    retirer la cle de l'arbre a
-  */
+void Detruire_Cle(Arbre234 *a, int cle) {
+    /*
+      retirer la cle de l'arbre a
+    */
 
-  return ;
+    return;
 }
 
 
+int main(int argc, char **argv) {
+    Arbre234 a;
 
-
-int main (int argc, char **argv)
-{
-  Arbre234 a ;
-
-  if (argc != 2)
-    {
-      fprintf (stderr, "il manque le parametre nom de fichier\n") ;
-      exit (-1) ;
+    if (argc != 2) {
+        fprintf(stderr, "il manque le parametre nom de fichier\n");
+        exit(-1);
     }
 
-  a = lire_arbre (argv [1]) ;
+    a = lire_arbre(argv[1]);
 
-  printf ("==== Afficher arbre ====\n") ;
-  
-  afficher_arbre (a, 0) ;
+    printf("==== Afficher arbre ====\n");
+
+    afficher_arbre(a, 0);
+
+    printf("%d\n", CleMax(a));
 
 }
